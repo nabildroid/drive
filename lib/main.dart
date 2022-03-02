6c62af35542a75cadadb0d995ad12c3206ac8c96
nabildroid <pni20156789@gmail.com>
2022-03-02 19:21:50 +0100

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,15 @@
-import 'package:drive/models/main.dart';
-import 'package:drive/repositories/files/files_factory.dart';
-import 'package:drive/repositories/files/local_files.dart';
-import 'package:drive/repositories/files/remote_files.dart';
-import 'package:drive/repositories/storage/storage_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'pages/home/home.dart';
+import 'app.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  final files =
-      FilesRepositoyFactory(LocalFilesRepository(), RemoteFilesRepository());
+  WidgetsFlutterBinding.ensureInitialized();
 
-  final screenshots = await files.createFolder(Folder([]));
-  final short = await files.link(screenshots, LinkedNode("dsd", "sds", "edsd"));
-  files.rename(short, "hello world");
-  files.share(short, [User("esdd", "dsdsd", "dsdsd")]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Home(),
-    );
-  }
 }
